5dd92a30171f13b4778c3d159e197fdf22289468
R17CS065 <r17cs065@Ubuntu-New037.rect.com>
2018-09-04 09:46:04 +0530

    4/09/2018

--- arrint.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void input(int a[]);
-void output(int a[]);
-void binarysearch(int a[]);
-void linearsearch(int a[]);
-void bubblesortasc(int a[]);
-void bubblesortdesc(int a[]); 
